fefe0b42bd7209fb22b602a8aaf2d6e616f365e1
Hugh <hscoreilly@gmail.com>
2022-05-26 23:03:29 +0100

    Added Favicon
    -Changed from default

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,7 @@ export default function Layout({ children }) {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.00"/>
                 <title>Hugh O'Reilly</title>
+                <link rel={"icon"} type="image/png" href="/WebsiteFavicon.png"/>
             </Head>
             <main className={""}>
                 <div className={"w-11/12 sm:w-9/12 mx-auto max-w-7xl"}>
