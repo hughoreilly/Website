782ddf95ada0746988061ee5d6722d666c351c73
Hugh <hscoreilly@gmail.com>
2022-05-26 10:58:31 +0100

    Pimped Navbar
    - Scale on hover looks corny so removed.
    - Current page is now a different colour on the navbar

--- components/layout.js
+++ components/layout.js
@@ -4,6 +4,7 @@ export default function Layout({ children }) {
     return (
         <>
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.00"/>
                 <title>Hugh O'Reilly</title>
             </Head>
             <main>{children}</main>
